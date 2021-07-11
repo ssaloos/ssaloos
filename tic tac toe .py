80e2eaa0347d8227f28df1c84b31eb72e2ff06a3
Sara <ssaloos@andrew.cmu.edu>
2021-07-11 23:02:25 +0300

    Update tic tac toe .py

--- tic tac toe .py
+++ tic tac toe .py
@@ -1,34 +1,17 @@
 print("Welcome to Tic Tac Toe!")
 character = input("Would you like to be X or O? ")
-options = ['X', 'O']
 print(f"[1] [2] [3]\n[4] [5] [6]\n[7] [8] [9]")
-XO = ["O, O, O, O, O, O, O, O, O"]
+XO = [" "," ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+element = int(input(f"Which place would you like to place {character}? "))
+
+if element >= 1 and element <= 9:
+    XO[element - 1] = character
+
+print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
 
 element = input(f"Which place would you like to place {character}? ")
 
-# if element == "1":
-#     print(f"[{character}] [] []\n[] [] []\n[] [] []")
-# elif element == "2":
-#     print(f"[] [{character}] []\n[] [] []\n[] [] []")
-# elif element == "3":
-#     print(f"[] [] [{character}]\n[] [] []\n[] [] []")
-# elif element == "4":
-#     print(f"[] [] []\n[{character}] [] []\n[] [] []")
-# elif element == "5":
-#     print(f"[] [] []\n[] [{character}] []\n[] [] []")
-# elif element == "6":
-#     print(f"[] [] []\n[] [] [{character}]\n[] [] []")
-# elif element == "7":
-#     print(f"[] [] []\n[] [] []\n[{character}] [] []")
-# elif element == "8":
-#     print(f"[] [] []\n[] [] []\n[] [{character}] []")
-# elif element == "9":
-#     print(f"[] [] []\n[] [] []\n[] [] [{character}]")
 
-for element in range (9):
-    print(f"[] [] []\n[] [] []\n[] [] []")
-    break
-element = input(f"Which place would you like to place {character}: ")
-
-
+print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
 
