ec8524e5d499ddde44e9192774d45bbbb417ecb5
Sara <ssaloos@andrew.cmu.edu>
2021-07-12 22:42:43 +0300

    Update tic tac toe .py

--- tic tac toe .py
+++ tic tac toe .py
@@ -1,4 +1,12 @@
+import random
 print("Welcome to Tic Tac Toe!")
+def check_empty():
+    empty_index = []
+    for index in range(9):
+        if XO[index] == " ":
+            empty_index.append(index)
+
+    return empty_index
 player_choice = {'X' : "", 'O' : ""}
 player = input("Player 1: would you like to be X or O? ")
 current_player = player
@@ -15,9 +23,26 @@ element = int(input(f"Which place would you like to place {player}? "))
 while element >= 1 and element <= 9:
     XO[element - 1] = player
     print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
-    random.randint(0,9)
-    print(random.randint)
-
+    player1_choice = random.choice(check_empty())
+    print(player1_choice)
+    random.randint(1,9)
+    element = player1_choice
+    XO[element - 1] = player1
+    print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
+    element = int(input(f"Which place would you like to place {player}? "))
+    player_choice = random.choice(check_empty())
+    element1 = player_choice
+    XO[element1 - 1] = player
+    print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
+    player1_choice = random.choice(check_empty())
+    print(player1_choice)
+    random.randint(1,9)
+    element = player1_choice
+    XO[element - 1] = player1
+    print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
+    element = int(input(f"Which place would you like to place {player}? "))
+    XO[element - 1] = player
+    print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
 def check_winner(current_player):
     win_comb = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
