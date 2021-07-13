4e859d705b8e7e76f19d5389c40ee81803541112
Sara <ssaloos@andrew.cmu.edu>
2021-07-14 00:17:46 +0300

    Update tic tac toe .py

--- tic tac toe .py
+++ tic tac toe .py
@@ -1,10 +1,11 @@
 import random
 import string
+import time
 print("Welcome to Tic Tac Toe!")
 XO = [" "," ", " ", " ", " ", " ", " ", " ", " ", " "]
 player_choice = {'X' : "", 'O' : ""}
-def print_tic_tac_toe():
-    print(" ")
+# def print_tic_tac_toe():
+#     print(" ")
 def check_empty():
     empty_index = []
     for index in range(9):
@@ -24,7 +25,7 @@ def check_winner():
         print("X wins!")
     elif player_choice("O") == win_comb:
         print("O wins!")
-# print(check_winner())
+    print(check_winner())
     # winner = xo_game(options[choice-1])
 player = input("Player 1: would you like to be X or O? ")
 cur_player = player
@@ -33,6 +34,9 @@ if cur_player == "X":
     player1 = "O"
 elif cur_player == "O":
     player1 = "X"
+elif cur_player != "X" or cur_player != "O":
+    print("You need to choose X or O!")
+    player = input("Player 1: would you like to be X or O? ")
 print(f"Player 2 (Computer) is : {player1}")
 print(f"[1] [2] [3]\n[4] [5] [6]\n[7] [8] [9]")
 element = int(input(f"Which place would you like to place {player}? "))
@@ -62,12 +66,12 @@ def xo_game(cur_player):
         if values[element-1] != ' ':
             print("Place filled. Try again!")
             continue
-        if check_win(player_pos, cur_player):
-            print_tic_tac_toe(values)
+        if check_winner(player_pos, cur_player):
+            print_tic_tac_toe()
             print("Player ", cur_player, " has won the game!")
             return cur_player
         if check_draw(player_pos):
-            print_tic_tac_toe(values)
+            print_tic_tac_toe()
             print("Game Drawn")
             return check_draw
         if cur_player == player:
@@ -78,7 +82,8 @@ while element >= 1 and element <= 9:
     XO[element - 1] = player
     print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
     player1_choice = random.choice(check_empty())
-    print(player1_choice)
+    print("Computer is choosing...")
+    time.sleep(0.5)
     random.randint(1,9)
     element = player1_choice
     XO[element - 1] = player1
@@ -88,7 +93,3 @@ while element >= 1 and element <= 9:
     element1 = player_choice
     XO[element1 - 1] = player
     print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
-
-
-
-
