abfa2662eaec65b2403fcbc6a220b1afa41baa00
Sara <ssaloos@andrew.cmu.edu>
2021-07-13 16:16:52 +0300

    Update tic tac toe .py

--- tic tac toe .py
+++ tic tac toe .py
@@ -1,6 +1,7 @@
 import random
 print("Welcome to Tic Tac Toe!")
-def print_tic_tac_toe(values):
+XO = [" "," ", " ", " ", " ", " ", " ", " ", " ", " "]
+def print_tic_tac_toe():
     print(f"[1] [2] [3]\n[4] [5] [6]\n[7] [8] [9]")
 def check_empty():
     empty_index = []
@@ -8,17 +9,21 @@ def check_empty():
         if XO[index] == " ":
             empty_index.append(index)
     return empty_index
-player_choice = {'X' : "", 'O' : ""}
-player = input("Player 1: would you like to be X or O? ")
-cur_player = player
-player1 = (" ")
-if cur_player == "X":
-    player1 = "O"
-elif cur_player == "O":
-    player1 = "X"
-print(f"Player 2 (Computer) is : {player1}")
-XO = [" "," ", " ", " ", " ", " ", " ", " ", " ", " "]
-element = int(input(f"Which place would you like to place {player}? "))
+def check_draw():
+    count = 0
+    for element in XO:
+        if element == " ":
+            count = count + 1
+    return count
+print(check_draw())
+def check_winner():
+    win_comb = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    if player_choice("X") == win_comb:
+        print("X wins!")
+    elif player_choice("O") == win_comb:
+        print("O wins!")
+# print(check_winner())
+    # winner = single_game(options[choice-1])
 def single_game(cur_player):
     values = [" " for x in range(9)]
     player_pos = {'X':[ ], 'O':[ ]}
@@ -57,6 +62,17 @@ def single_game(cur_player):
             cur_player = player1
         else:
             cur_player = player
+player_choice = {'X' : "", 'O' : ""}
+player = input("Player 1: would you like to be X or O? ")
+print(f"[1] [2] [3]\n[4] [5] [6]\n[7] [8] [9]")
+cur_player = player
+player1 = (" ")
+if cur_player == "X":
+    player1 = "O"
+elif cur_player == "O":
+    player1 = "X"
+print(f"Player 2 (Computer) is : {player1}")
+element = int(input(f"Which place would you like to place {player}? "))
 while element >= 1 and element <= 9:
     XO[element - 1] = player
     print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
@@ -71,19 +87,7 @@ while element >= 1 and element <= 9:
     element1 = player_choice
     XO[element1 - 1] = player
     print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
-def check_winner(cur_player):
-    win_comb = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-    if player_choice("X") == win_comb:
-        print("X wins!")
-    elif player_choice("O") == win_comb:
-        print("O wins!")
-def check_draw(player_pos):
-    if len(player_pos["X"]) + len(player_pos["O"]) == 9:
-        return True
-    return False
-    winner = single_game(options[choice-1])
-    if winner != 'D' :
-        player_won = player_choice[winner]
+
 
 
 
