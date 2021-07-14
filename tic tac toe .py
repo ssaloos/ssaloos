cff9ded994781cee6992e1aa9f536ee79464b006
Sara <ssaloos@andrew.cmu.edu>
2021-07-14 18:19:57 +0300

    Update tic tac toe .py

--- tic tac toe .py
+++ tic tac toe .py
@@ -1,11 +1,12 @@
 import random
-import string
 import time
 print("Welcome to Tic Tac Toe!")
 XO = [" "," ", " ", " ", " ", " ", " ", " ", " ", " "]
-player_choice = {'X' : "", 'O' : ""}
-# def print_tic_tac_toe():
-#     print(" ")
+player_pos = {"X":[], "O":[]}
+empty_list = []
+player_choice = {"X": "", "O" : ""}
+def print_tic_tac_toe():
+    print(" ")
 def check_empty():
     empty_index = []
     for index in range(9):
@@ -15,81 +16,89 @@ def check_empty():
 def check_draw():
     count = 0
     for element in XO:
-        if element == " ":
+        if element != " ":
             count = count + 1
     return count
-print(check_draw())
+check_draw()
 def check_winner():
-    win_comb = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-    if player_choice("X") == win_comb:
-        print("X wins!")
-    elif player_choice("O") == win_comb:
-        print("O wins!")
-    print(check_winner())
-    # winner = xo_game(options[choice-1])
+    if XO[0] == "X" and XO[1] == "X" and XO[2] == "X":
+        print ("X won!")
+        return True
+    elif XO[0] == "O" and XO[1] == "O" and XO[2] == "O":
+        print ("O won!")
+        return True
+    elif XO[0] == "X" and XO[3] == "X" and XO[6] == "X":
+        print ("X won!")
+        return True
+    elif XO[0] == "O" and XO[3] == "O" and XO[6] == "O":
+        print ("O won!")
+        return True
+    elif XO[3] == "X" and XO[4] == "X" and XO[5] == "X":
+        print ("X won!")
+        return True
+    elif XO[3] == "O" and XO[4] == "O" and XO[5] == "O":
+        print ("O won!")
+        return True
+    elif XO[6] == "X" and XO[7] == "X" and XO[8] == "X":
+        print ("X won!")
+        return True
+    elif XO[6] == "O" and XO[7] == "O" and XO[8] == "O":
+        print ("O won!")
+        return True
+    elif XO[1] == "X" and XO[4] == "X" and XO[7] == "X":
+        print ("X won!")
+        return True    
+    elif XO[1] == "O" and XO[4] == "O" and XO[7] == "O":
+        print ("O won!")
+        return True
+    elif XO[2] == "X" and XO[5] == "X" and XO[8] == "X":
+        print ("X won!")
+        return True
+    elif XO[2] == "O" and XO[5] == "O" and XO[8] == "O":
+        print ("O won!")
+        return True
+    elif XO[0] == "X" and XO[4] == "X" and XO[8] == "X":
+        print ("X won!")
+        return True
+    elif XO[0] == "O" and XO[4] == "O" and XO[8] == "O":
+        print ("O won!")
+        return True
+    elif XO[2] == "X" and XO[4] == "X" and XO[6] == "X":
+        print ("X won!")
+        return True
+    elif XO[2] == "O" and XO[4] == "O" and XO[6] == "O":
+        print ("O won!")
+        return True
+    return False 
+check_winner()
 player = input("Player 1: would you like to be X or O? ")
 cur_player = player
-player1 = (" ")
-if cur_player == "X":
+if player == "X":
     player1 = "O"
-elif cur_player == "O":
+elif player == "O":
     player1 = "X"
-elif cur_player != "X" or cur_player != "O":
-    print("You need to choose X or O!")
-    player = input("Player 1: would you like to be X or O? ")
 print(f"Player 2 (Computer) is : {player1}")
 print(f"[1] [2] [3]\n[4] [5] [6]\n[7] [8] [9]")
 element = int(input(f"Which place would you like to place {player}? "))
-def xo_game(cur_player):
-    values = [" " for x in range(9)]
-    player_pos = {"X":[ ], "O":[ ]}
-    while True:
-        print(f"[1] [2] [3]\n[4] [5] [6]\n[7] [8] [9]")
-        player = input("Player 1: would you like to be X or O? ")
-        cur_player = player
-        player1 = (" ")
-        if cur_player == "X":
-            player1 = "O"
-        elif cur_player == "O":
-            player1 = "X"
-        print(f"Player 2 (Computer) is : {player1}")
-        XO = [" "," ", " ", " ", " ", " ", " ", " ", " ", " "]
-        element = int(input(f"Which place would you like to place {player}? "))
-        if element < 1 or element > 9:
-            print("Choose a number from 1-9!")
-            continue
-        if values[element-1] != " ":
-            print("Place already filled. Try again!")
-            continue
-        values[element-1] = cur_player
-        player_pos[cur_player].append(element)
-        if values[element-1] != ' ':
-            print("Place filled. Try again!")
-            continue
-        if check_winner(player_pos, cur_player):
-            print_tic_tac_toe()
-            print("Player ", cur_player, " has won the game!")
-            return cur_player
-        if check_draw(player_pos):
-            print_tic_tac_toe()
-            print("Game Drawn")
-            return check_draw
-        if cur_player == player:
-            cur_player == player1
-        else:
-            cur_player == player
+XO[element - 1] = player
+print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
 while element >= 1 and element <= 9:
-    XO[element - 1] = player
-    print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
+    if element >= 1 and element <= 9:
+        empty_list.append(element)
     player1_choice = random.choice(check_empty())
     print("Computer is choosing...")
-    time.sleep(0.5)
-    random.randint(1,9)
-    element = player1_choice
-    XO[element - 1] = player1
+    time.sleep(0.6)
+    element1 = player1_choice
+    XO[element1] = player1
     print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
     element = int(input(f"Which place would you like to place {player}? "))
-    player_choice = random.choice(check_empty())
-    element1 = player_choice
-    XO[element1 - 1] = player
+    XO[element - 1] = player
     print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
+    element1 = player1_choice
+    XO[element1] = player1
+    print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
+    player1_choice = random.choice(check_empty())
+    check_draw()
+    check_winner()
+    if check_winner():
+        break 
