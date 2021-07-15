062126804c018d698a6da4c69ab988d5f3fdf1c0
ssaloos <87118770+ssaloos@users.noreply.github.com>
2021-07-15 15:40:20 +0300

    Update tic tac toe .py

--- tic tac toe .py
+++ tic tac toe .py
@@ -2,24 +2,13 @@ import random
 import time
 print("Welcome to Tic Tac Toe!")
 XO = [" "," ", " ", " ", " ", " ", " ", " ", " ", " "]
-player_pos = {"X":[], "O":[]}
 empty_list = []
-player_choice = {"X": "", "O" : ""}
-def print_tic_tac_toe():
-    print(" ")
 def check_empty():
     empty_index = []
     for index in range(9):
         if XO[index] == " ":
             empty_index.append(index)
     return empty_index
-def check_draw():
-    count = 0
-    for element in XO:
-        if element != " ":
-            count = count + 1
-    return count
-check_draw()
 def check_winner():
     if XO[0] == "X" and XO[1] == "X" and XO[2] == "X":
         print ("X won!")
@@ -47,7 +36,7 @@ def check_winner():
         return True
     elif XO[1] == "X" and XO[4] == "X" and XO[7] == "X":
         print ("X won!")
-        return True    
+        return True
     elif XO[1] == "O" and XO[4] == "O" and XO[7] == "O":
         print ("O won!")
         return True
@@ -69,29 +58,37 @@ def check_winner():
     elif XO[2] == "O" and XO[4] == "O" and XO[6] == "O":
         print ("O won!")
         return True
-    return False 
+    return False
+def check_draw():
+    count = 0
+    for element in XO:
+        if element != " ":
+            count = count + 1
+    return count
+check_draw()
 check_winner()
-player = input("Player 1: would you like to be X or O? ")
-cur_player = player
+name = input("Player 1, what's your name? ")
+player = input(f"Hi, {name}! Would you like to be X or O? ")
 if player == "X":
     player1 = "O"
 elif player == "O":
     player1 = "X"
 print(f"Player 2 (Computer) is : {player1}")
 print(f"[1] [2] [3]\n[4] [5] [6]\n[7] [8] [9]")
-element = int(input(f"Which place would you like to place {player}? "))
+element = int(input(f"{name}, which place would you like to place {player}? "))
 XO[element - 1] = player
 print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
 while element >= 1 and element <= 9:
-    if element >= 1 and element <= 9:
-        empty_list.append(element)
+    empty_list.append(element)
     player1_choice = random.choice(check_empty())
     print("Computer is choosing...")
     time.sleep(0.6)
     element1 = player1_choice
     XO[element1] = player1
     print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
-    element = int(input(f"Which place would you like to place {player}? "))
+    if check_winner():
+        break
+    element = int(input(f"{name}, which place would you like to place {player}? "))
     XO[element - 1] = player
     print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
     element1 = player1_choice
@@ -99,6 +96,5 @@ while element >= 1 and element <= 9:
     print(f"[{XO[0]}] [{XO[1]}] [{XO[2]}]\n[{XO[3]}] [{XO[4]}] [{XO[5]}]\n[{XO[6]}] [{XO[7]}] [{XO[8]}]")
     player1_choice = random.choice(check_empty())
     check_draw()
-    check_winner()
     if check_winner():
-        break 
+        break
